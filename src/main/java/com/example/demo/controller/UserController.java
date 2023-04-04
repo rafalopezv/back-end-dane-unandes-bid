11ac053fc5a668bd8997a3ab42cd291f4d227e97
Rafa López V <lopezvalverde.rafael@gmail.com>
2023-04-04 14:35:59 -0500

    prueba no contagio repo original

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+import com.example.demo.exception.UserNotFoundException;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+//@CrossOrigin("https://unandes-prueba.vercel.app")
+
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUSer(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User>  getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        if(!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return  "User with id "+id+ " has been deleted succesfully.";
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,494 @@
+package com.example.demo.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
+    private String al_p1_n;
+    private String al_p2_c;
+    private String al_p3_m1;
+    private String al_p3_m2;
+    private String al_p3_m3;
+    private String al_p3_m4;
+    private String al_p3_m_otro;
+    private String al_p3_m_otro_texto;
+
+    private String al_p4_c;
+    private String al_p5_n1_cop;
+    private String al_p5_n2_cop;
+    private String al_p5_n3_cop;
+    private String al_p5_n4_cop;
+    private String al_p5_n5_cop;
+    private String al_p5_n6_cop;
+
+    private String al_p5_n1_kg;
+    private String al_p5_n2_kg;
+    private String al_p5_n3_kg;
+    private String al_p5_n4_kg;
+    private String al_p5_n5_kg;
+    private String al_p5_n6_kg;
+    private String al_p5_otro_cop_numero;
+    private String al_p5_otro_kg_numero;
+    private String al_p5_otro_kg_texto;
+    private String al_p6_n1_kg;
+
+    private String al_p6_n2_kg;
+
+    private String al_p6_n3_kg;
+
+    private String al_p6_n4_kg;
+
+    private String al_p6_n5_kg;
+
+    private String al_p6_n6_kg;
+
+    private String al_p6_otro_kg_numero;
+
+    private String al_p6_otro_kg_texto;
+
+    private String al_p7_m1;
+    private String al_p7_m2;
+    private String al_p7_m3;
+    private String al_p7_m4;
+    private String al_p7_m5;
+    private String al_p7_m6;
+    private String al_p7_m7;
+
+    private String al_p7_otro;
+
+    private String al_p7_otro_texto;
+
+    private String al_p8_m1;
+
+    private String al_p8_m2;
+    private String al_p8_m3;
+    private String al_p8_m4;
+    private String al_p8_m5;
+    private String al_p8_otro;
+    private String al_p8_otro_texto;
+
+
+    public String getAl_p1_n() {
+        return al_p1_n;
+    }
+
+    public void setAl_p1_n(String al_p1_n) {
+        this.al_p1_n = al_p1_n;
+    }
+
+    public String getAl_p2_c() {
+        return al_p2_c;
+    }
+
+    public void setAl_p2_c(String al_p2_c) {
+        this.al_p2_c = al_p2_c;
+    }
+
+    public String getAl_p3_m1() {
+        return al_p3_m1;
+    }
+
+    public void setAl_p3_m1(String al_p3_m1) {
+        this.al_p3_m1 = al_p3_m1;
+    }
+
+    public String getAl_p3_m2() {
+        return al_p3_m2;
+    }
+
+    public void setAl_p3_m2(String al_p3_m2) {
+        this.al_p3_m2 = al_p3_m2;
+    }
+
+    public String getAl_p3_m3() {
+        return al_p3_m3;
+    }
+
+    public void setAl_p3_m3(String al_p3_m3) {
+        this.al_p3_m3 = al_p3_m3;
+    }
+
+    public String getAl_p3_m4() {
+        return al_p3_m4;
+    }
+
+    public void setAl_p3_m4(String al_p3_m4) {
+        this.al_p3_m4 = al_p3_m4;
+    }
+
+    public String getAl_p3_m_otro() {
+        return al_p3_m_otro;
+    }
+
+    public void setAl_p3_m_otro(String al_p3_m_otro) {
+        this.al_p3_m_otro = al_p3_m_otro;
+    }
+
+    public String getAl_p3_m_otro_texto() {
+        return al_p3_m_otro_texto;
+    }
+
+    public void setAl_p3_m_otro_texto(String al_p3_m_otro_texto) {
+        this.al_p3_m_otro_texto = al_p3_m_otro_texto;
+    }
+
+    public String getAl_p4_c() {
+        return al_p4_c;
+    }
+
+    public void setAl_p4_c(String al_p4_c) {
+        this.al_p4_c = al_p4_c;
+    }
+
+    public String getAl_p5_n1_cop() {
+        return al_p5_n1_cop;
+    }
+
+    public void setAl_p5_n1_cop(String al_p5_n1_cop) {
+        this.al_p5_n1_cop = al_p5_n1_cop;
+    }
+
+    public String getAl_p5_n2_cop() {
+        return al_p5_n2_cop;
+    }
+
+    public void setAl_p5_n2_cop(String al_p5_n2_cop) {
+        this.al_p5_n2_cop = al_p5_n2_cop;
+    }
+
+    public String getAl_p5_n3_cop() {
+        return al_p5_n3_cop;
+    }
+
+    public void setAl_p5_n3_cop(String al_p5_n3_cop) {
+        this.al_p5_n3_cop = al_p5_n3_cop;
+    }
+
+    public String getAl_p5_n4_cop() {
+        return al_p5_n4_cop;
+    }
+
+    public void setAl_p5_n4_cop(String al_p5_n4_cop) {
+        this.al_p5_n4_cop = al_p5_n4_cop;
+    }
+
+    public String getAl_p5_n5_cop() {
+        return al_p5_n5_cop;
+    }
+
+    public void setAl_p5_n5_cop(String al_p5_n5_cop) {
+        this.al_p5_n5_cop = al_p5_n5_cop;
+    }
+
+    public String getAl_p5_n6_cop() {
+        return al_p5_n6_cop;
+    }
+
+    public void setAl_p5_n6_cop(String al_p5_n6_cop) {
+        this.al_p5_n6_cop = al_p5_n6_cop;
+    }
+
+    public String getAl_p5_n1_kg() {
+        return al_p5_n1_kg;
+    }
+
+    public void setAl_p5_n1_kg(String al_p5_n1_kg) {
+        this.al_p5_n1_kg = al_p5_n1_kg;
+    }
+
+    public String getAl_p5_n2_kg() {
+        return al_p5_n2_kg;
+    }
+
+    public void setAl_p5_n2_kg(String al_p5_n2_kg) {
+        this.al_p5_n2_kg = al_p5_n2_kg;
+    }
+
+    public String getAl_p5_n3_kg() {
+        return al_p5_n3_kg;
+    }
+
+    public void setAl_p5_n3_kg(String al_p5_n3_kg) {
+        this.al_p5_n3_kg = al_p5_n3_kg;
+    }
+
+    public String getAl_p5_n4_kg() {
+        return al_p5_n4_kg;
+    }
+
+    public void setAl_p5_n4_kg(String al_p5_n4_kg) {
+        this.al_p5_n4_kg = al_p5_n4_kg;
+    }
+
+    public String getAl_p5_n5_kg() {
+        return al_p5_n5_kg;
+    }
+
+    public void setAl_p5_n5_kg(String al_p5_n5_kg) {
+        this.al_p5_n5_kg = al_p5_n5_kg;
+    }
+
+    public String getAl_p5_n6_kg() {
+        return al_p5_n6_kg;
+    }
+
+    public void setAl_p5_n6_kg(String al_p5_n6_kg) {
+        this.al_p5_n6_kg = al_p5_n6_kg;
+    }
+
+    public String getAl_p5_otro_cop_numero() {
+        return al_p5_otro_cop_numero;
+    }
+
+    public void setAl_p5_otro_cop_numero(String al_p5_otro_cop_numero) {
+        this.al_p5_otro_cop_numero = al_p5_otro_cop_numero;
+    }
+
+    public String getAl_p5_otro_kg_numero() {
+        return al_p5_otro_kg_numero;
+    }
+
+    public void setAl_p5_otro_kg_numero(String al_p5_otro_kg_numero) {
+        this.al_p5_otro_kg_numero = al_p5_otro_kg_numero;
+    }
+
+    public String getAl_p5_otro_kg_texto() {
+        return al_p5_otro_kg_texto;
+    }
+
+    public void setAl_p5_otro_kg_texto(String al_p5_otro_kg_texto) {
+        this.al_p5_otro_kg_texto = al_p5_otro_kg_texto;
+    }
+
+    public String getAl_p6_n1_kg() {
+        return al_p6_n1_kg;
+    }
+
+    public void setAl_p6_n1_kg(String al_p6_n1_kg) {
+        this.al_p6_n1_kg = al_p6_n1_kg;
+    }
+
+    public String getAl_p6_n2_kg() {
+        return al_p6_n2_kg;
+    }
+
+    public void setAl_p6_n2_kg(String al_p6_n2_kg) {
+        this.al_p6_n2_kg = al_p6_n2_kg;
+    }
+
+    public String getAl_p6_n3_kg() {
+        return al_p6_n3_kg;
+    }
+
+    public void setAl_p6_n3_kg(String al_p6_n3_kg) {
+        this.al_p6_n3_kg = al_p6_n3_kg;
+    }
+
+    public String getAl_p6_n4_kg() {
+        return al_p6_n4_kg;
+    }
+
+    public void setAl_p6_n4_kg(String al_p6_n4_kg) {
+        this.al_p6_n4_kg = al_p6_n4_kg;
+    }
+
+    public String getAl_p6_n5_kg() {
+        return al_p6_n5_kg;
+    }
+
+    public void setAl_p6_n5_kg(String al_p6_n5_kg) {
+        this.al_p6_n5_kg = al_p6_n5_kg;
+    }
+
+    public String getAl_p6_n6_kg() {
+        return al_p6_n6_kg;
+    }
+
+    public void setAl_p6_n6_kg(String al_p6_n6_kg) {
+        this.al_p6_n6_kg = al_p6_n6_kg;
+    }
+
+    public String getAl_p6_otro_kg_numero() {
+        return al_p6_otro_kg_numero;
+    }
+
+    public void setAl_p6_otro_kg_numero(String al_p6_otro_kg_numero) {
+        this.al_p6_otro_kg_numero = al_p6_otro_kg_numero;
+    }
+
+    public String getAl_p6_otro_kg_texto() {
+        return al_p6_otro_kg_texto;
+    }
+
+    public void setAl_p6_otro_kg_texto(String al_p6_otro_kg_texto) {
+        this.al_p6_otro_kg_texto = al_p6_otro_kg_texto;
+    }
+
+    public String getAl_p7_m1() {
+        return al_p7_m1;
+    }
+
+    public void setAl_p7_m1(String al_p7_m1) {
+        this.al_p7_m1 = al_p7_m1;
+    }
+
+    public String getAl_p7_m2() {
+        return al_p7_m2;
+    }
+
+    public void setAl_p7_m2(String al_p7_m2) {
+        this.al_p7_m2 = al_p7_m2;
+    }
+
+    public String getAl_p7_m3() {
+        return al_p7_m3;
+    }
+
+    public void setAl_p7_m3(String al_p7_m3) {
+        this.al_p7_m3 = al_p7_m3;
+    }
+
+    public String getAl_p7_m4() {
+        return al_p7_m4;
+    }
+
+    public void setAl_p7_m4(String al_p7_m4) {
+        this.al_p7_m4 = al_p7_m4;
+    }
+
+    public String getAl_p7_m5() {
+        return al_p7_m5;
+    }
+
+    public void setAl_p7_m5(String al_p7_m5) {
+        this.al_p7_m5 = al_p7_m5;
+    }
+
+    public String getAl_p7_m6() {
+        return al_p7_m6;
+    }
+
+    public void setAl_p7_m6(String al_p7_m6) {
+        this.al_p7_m6 = al_p7_m6;
+    }
+
+    public String getAl_p7_m7() {
+        return al_p7_m7;
+    }
+
+    public void setAl_p7_m7(String al_p7_m7) {
+        this.al_p7_m7 = al_p7_m7;
+    }
+
+    public String getAl_p7_otro() {
+        return al_p7_otro;
+    }
+
+    public void setAl_p7_otro(String al_p7_otro) {
+        this.al_p7_otro = al_p7_otro;
+    }
+
+    public String getAl_p7_otro_texto() {
+        return al_p7_otro_texto;
+    }
+
+    public void setAl_p7_otro_texto(String al_p7_otro_texto) {
+        this.al_p7_otro_texto = al_p7_otro_texto;
+    }
+
+    public String getAl_p8_m1() {
+        return al_p8_m1;
+    }
+
+    public void setAl_p8_m1(String al_p8_m1) {
+        this.al_p8_m1 = al_p8_m1;
+    }
+
+    public String getAl_p8_m2() {
+        return al_p8_m2;
+    }
+
+    public void setAl_p8_m2(String al_p8_m2) {
+        this.al_p8_m2 = al_p8_m2;
+    }
+
+    public String getAl_p8_m3() {
+        return al_p8_m3;
+    }
+
+    public void setAl_p8_m3(String al_p8_m3) {
+        this.al_p8_m3 = al_p8_m3;
+    }
+
+    public String getAl_p8_m4() {
+        return al_p8_m4;
+    }
+
+    public void setAl_p8_m4(String al_p8_m4) {
+        this.al_p8_m4 = al_p8_m4;
+    }
+
+    public String getAl_p8_m5() {
+        return al_p8_m5;
+    }
+
+    public void setAl_p8_m5(String al_p8_m5) {
+        this.al_p8_m5 = al_p8_m5;
+    }
+
+    public String getAl_p8_otro() {
+        return al_p8_otro;
+    }
+
+    public void setAl_p8_otro(String al_p8_otro) {
+        this.al_p8_otro = al_p8_otro;
+    }
+
+    public String getAl_p8_otro_texto() {
+        return al_p8_otro_texto;
+    }
+
+    public void setAl_p8_otro_texto(String al_p8_otro_texto) {
+        this.al_p8_otro_texto = al_p8_otro_texto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
